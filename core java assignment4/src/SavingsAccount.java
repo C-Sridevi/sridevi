c54e5697677314377f3f4ab72cbb3497f68a9ef7
C-Sridevi <95856842+C-Sridevi@users.noreply.github.com>
2021-12-23 21:58:35 +0530

    Add files via upload

---
+++ core java assignment4/src/SavingsAccount.java
@@ -0,0 +1,6 @@
+
+public interface SavingsAccount {
+	public void deposite(double amount) throws IllegalBankTransactionException;
+	 public double withdraw(double amount) throws InsufficientBalanceException;
+	 public void balanceEnquiry();
+}
